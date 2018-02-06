4fa84cf6eeb726317a124596a602facde9fef695
Silent-Era <junkyard@abv.bg>
2018-02-06 23:11:52 +0200

    html and image loading

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -14,12 +14,44 @@ module.exports = {
 	},
 	module: {
 		rules: [
-		{test: /\.css$/,
-			use: [
-			{loader: 'style-loader'},
-			{loader: 'css-loader'}
-			]
-		}
+			{
+				test: /\.css$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'}
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: "[name].html"
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'html-loader',
+						options: {
+							attrs: ['img:src']
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpg|gif|png)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name]-[hash:8].[ext]'
+						}
+					}
+				]
+			}
 		]
 	}
 }
