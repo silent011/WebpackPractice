ddc8818924c6a185fc2feac31f1a1116e80a98d9
Silent-Era <junkyard@abv.bg>
2018-02-20 23:27:38 +0200

    dynamic importing

--- ForProduction/src/components/Gallery.js
+++ ForProduction/src/components/Gallery.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+const getBundle = () => {
+    import(/* webpackChunkName: "lodash" */'lodash').then( _ => {
+        console.log('imported', _)
+    })
+}
+
 export default () => (
     <section className="gallery">
-        <h1>Gallry </h1>
+        <h1 onClick={getBundle}>Gallery </h1>
         <p>Hi, This is the gallery page</p>
     </section>
 )
